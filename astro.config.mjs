e44c2abec6eea289a27cc2697a8fe3588cfcb8b9
Jack-S-H <58410600+Jack-S-H@users.noreply.github.com>
2025-05-19 23:35:51 +0800

    重构页面结构，添加导航和页脚组件，更新样式以提升一致性，优化菜单交互功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://astronaut.github.io',
     base:'first-web',
-    build: {
-        assetsPrefix: 'first-web'
-    }
 });
