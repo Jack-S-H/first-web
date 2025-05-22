381ca0b6bbfc55f9736a82d01cf8fb510104c6a7
Jack-S-H <58410600+Jack-S-H@users.noreply.github.com>
2025-05-22 14:35:17 +0800

    更新 README.md 文件标题，修正 astro.config.mjs 中的 base 路径，优化 Footer 组件样式，调整 MarkdownPostLayout 组件中的标签链接，注释掉首页中的 Greeting 组件，修正标签索引页面中的标签链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Jack-S-H.github.io/',
-  base:'first-web',
+  base:'/first-web',
   integrations: [preact()],
 });
